18c527e76de3ebadfa314505ac1f6d407ba86307
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-04-21 17:13:25 +0600

    update#9.

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -2,20 +2,19 @@ import React from 'react';
 import './Header.css'
 import Navigation from '../Navigation/Navigation';
 
-{/* <div className='header-container'>
-            <Navigation></Navigation>
-            <div className="text-container">
-                <h1>Event Fairy</h1>
-                <h4>All Events Should be memorable!</h4>
-            </div>
-
-        </div> */}
+{/*  */ }
 
 const Header = () => {
     return (
         <>
             <Navigation></Navigation>
+            <div className='header-container'>
+            <div className="text-container">
+                <h1>Event Fairy</h1>
+                <h4>All Events Should be memorable!</h4>
+            </div>
 
+            </div>
         </>
     );
 };
